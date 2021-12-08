3b297984c0df3b48f7628d20aa747840262d345f
JaeGao <92616764+JaeGao@users.noreply.github.com>
2021-12-09 00:12:10 +0800

    Squashed commit of the following:
    commit 517946ffe478d0be4e6cf4d26b146a5b1e6d92f1
    Merge: f7301b1 048f116
    Author: satya-ktirtha <70558041+satya-ktirtha@users.noreply.github.com>
    Date:   Thu Dec 9 00:07:27 2021 +0800
    
        Merge pull request #4 from TonyZTYang/navbar
    
        Navbar
    
    commit 048f11649c114ce02d1ab4031097a8f6260fcca8
    Author: satya-ktirtha <satya.ktirtha@gmail.com>
    Date:   Thu Dec 9 00:05:43 2021 +0800
    
        commit before merging to master
    
    commit 7b5d8990a86928b5e254838046072e5438d6a63a
    Author: satya-ktirtha <satya.ktirtha@gmail.com>
    Date:   Wed Dec 8 23:36:09 2021 +0800
    
        added menuitem and changed how nav item is added\
    
    commit a5c9c5806238913d9779399bb7b924e725a82327
    Author: satya-ktirtha <satya.ktirtha@gmail.com>
    Date:   Wed Dec 8 17:15:12 2021 +0800
    
        UPDATED navbar ADDED content
    
    commit 1a6e95c77861fdcf58450820f43dd2a80baf5be7
    Author: satya-ktirtha <satya.ktirtha@gmail.com>
    Date:   Tue Dec 7 22:46:10 2021 +0800
    
        asdfsd
    
    commit 11e9c7736f7235eeb4ebd49476d6f44060cf7391
    Merge: 36772a8 236217c
    Author: satya-ktirtha <satya.ktirtha@gmail.com>
    Date:   Tue Dec 7 22:45:00 2021 +0800
    
        adfasd
    
    commit 36772a859813220b294bf94ea17b443fb78f3298
    Author: satya-ktirtha <satya.ktirtha@gmail.com>
    Date:   Tue Dec 7 22:39:24 2021 +0800
    
        deleted profile.js
    
    commit 236217c7887c7aea6d0c234024ea43eaca56606b
    Author: Shirley Liu <sl6895@nyu.edu>
    Date:   Sun Dec 5 19:29:44 2021 +0800
    
        added dashboard
    
    commit 87942c28dc8df66ffd263547560be5fc1501a93c
    Author: satya-ktirtha <satya.ktirtha@gmail.com>
    Date:   Sun Dec 5 19:25:54 2021 +0800
    
        Remove octo
    
    commit 733509015aff3bed57a28577aa5a15bee44c51c2
    Author: Shirley Liu <sl6895@nyu.edu>
    Date:   Sun Dec 5 19:17:32 2021 +0800
    
        added test2
    
    commit 5a239da3fab4b6675dfa6393c7277c6f43c0d1f1
    Author: satya-ktirtha <satya.ktirtha@gmail.com>
    Date:   Sun Dec 5 19:13:13 2021 +0800
    
        remove test.txt
    
    commit 7d4badf3eaa56dce6663d3dac8bc8b73ba8e0ddf
    Author: satya-ktirtha <satya.ktirtha@gmail.com>
    Date:   Sun Dec 5 19:12:40 2021 +0800
    
        add test.txt
    
    commit ad44e1ee5c23bcfbf0157af89d30253a7f660b39
    Author: satya-ktirtha <satya.ktirtha@gmail.com>
    Date:   Sun Dec 5 19:08:07 2021 +0800
    
        test commit
    
    commit ccccdf2be5d590f836d82e355363a607109876a7
    Author: Shirley Liu <sl6895@nyu.edu>
    Date:   Sun Dec 5 19:06:38 2021 +0800
    
        added test to navbar
    
    commit 4096033e714599e01252a539b9acb3056f157d58
    Author: satya-ktirtha <satya.ktirtha@gmail.com>
    Date:   Sun Dec 5 18:58:09 2021 +0800
    
        navbar first commit

---
+++ front-end/src/components/sidebar.js
@@ -0,0 +1,52 @@
+import "./css/sidebar.css"
+
+import React from "react";
+
+import Container from "react-bootstrap/Container"
+import Button from "react-bootstrap/Button"
+
+import Hello from "./hello"
+
+class SideBar extends React.Component {
+
+    static instance = null;
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            prevPage: null,
+            currentPage: null // TODO: start page will be dashboard
+        }
+
+        this.setContent = props.setContent.bind();
+        SideBar.instance = this;
+    }
+
+    changePage(content) {
+        return content;
+    }
+
+    render() {
+        return (
+            <Container id="container_nav_bar">
+                <MenuItem pageName="Hello" content={Hello}/>
+            </Container>
+        );
+    }
+}
+
+class MenuItem extends React.Component {
+
+    static setContent;
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (<Button className="button_nav_menu" onClick={() => {SideBar.instance.setContent(this.props.content)}}>{this.props.pageName}</Button>);
+    }
+}
+
+export default SideBar;
